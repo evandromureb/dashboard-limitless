1a6eb0a889314bb505204f4055366af738b58189
Evandro Mureb <evandrommureb@gmail.com>
2022-04-25 17:39:41 -0300

    DL-11 Configure layout resend e-mail

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,3 +1,98 @@
+@extends('layouts.auth')
+
+@section('auth')
+
+	<form 
+        class="form w-100 fv-plugins-bootstrap5 fv-plugins-framework"
+        novalidate="novalidate"
+        method="POST"
+        action="{{ route('password.email') }}"    
+    >
+    @csrf
+
+        <div class="text-center mb-10">
+
+            <h1 class="text-dark mb-3">
+                Cadastrar nova senha
+            </h1>
+        
+            <div class="text-gray-400 fw-bold fs-4">
+                Já cadastrou uma nova senha?        
+                <a href="{{  route('login') }}" class="link-primary fw-bolder">
+                    Efetuar login
+                </a>
+            </div>
+            
+        </div>
+        
+        <div class="mb-10 fv-row fv-plugins-icon-container" data-kt-password-meter="true">
+        
+            <div class="mb-1">
+            
+                <label class="form-label fw-bolder text-dark fs-6">
+                    Senha
+                </label>
+
+                <div class="position-relative mb-3">
+
+					<input
+						class="form-control form-control-lg form-control-solid"
+						type="password"
+						placeholder=""
+						name="password"
+						autocomplete="off"
+					>
+					<span class="btn btn-sm btn-icon position-absolute translate-middle top-50 end-0 me-n2"
+						  data-kt-password-meter-control="visibility"
+					>
+						<i class="bi bi-eye-slash fs-2"></i>
+						<i class="bi bi-eye fs-2 d-none"></i>
+					</span>
+
+				</div>
+
+				<div class="d-flex align-items-center mb-3" data-kt-password-meter-control="highlight">
+					<div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
+					<div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
+					<div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
+					<div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px"></div>
+				</div>
+            </div>
+            
+            <div class="fv-plugins-message-container invalid-feedback">
+                Erro
+            </div>
+            
+        </div>
+        
+        <div class="fv-row mb-10 fv-plugins-icon-container">
+
+            <label class="form-label fw-bolder text-dark fs-6">
+                Confirmar senha
+            </label>
+            <input 
+                class="form-control form-control-lg form-control-solid" 
+                type="password" 
+                placeholder="" 
+                name="password_confirmation"
+                autocomplete="off"
+            >
+            <div class="fv-plugins-message-container invalid-feedback">
+                Erro
+            </div>
+        </div>
+        
+        <div class="text-center">
+            <button type="submit" class="btn btn-lg btn-primary fw-bolder">
+                Cadastrar
+            </button>
+        </div>
+        
+    </form>
+
+@endsection
+
+{{--
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
@@ -46,3 +141,4 @@
         </form>
     </x-auth-card>
 </x-guest-layout>
+--}}
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,3 +1,46 @@
+	<div class="pt-lg-10 mb-10">
+
+		<h1 class="fw-bolder fs-2qx text-gray-800 mb-7">
+			Verifique seu e-mail
+		</h1>
+		@if (session('status') == 'verification-link-sent')
+			<div class="alert bg-light-primary d-flex align-content-end">
+				<span class="fw-bold ">
+					Mensagem reenviada para o seu e-mail cadastrado.
+				</span>
+			</div>
+		@endif
+
+		<div class="fs-3 fw-bold text-muted mb-10">
+			Foi enviado uma mensagem com um link de verificação de sua conta.
+			<br>
+			Verifique seu e-mail para confirmar sua conta.
+		</div>
+
+		<div class="text-center mb-10">
+			<a href="{{ route('login') }}" class="btn btn-lg btn-primary fw-bolder">
+				Efetuar login
+			</a>
+		</div>
+
+
+		<form method="POST" action="{{ route('verification.send') }}">
+			@csrf
+
+			<div class="fs-5">
+				<span class="fw-bold text-gray-700">
+					Não recebeu a mensagem?</span>
+				<button type="submit" class="link-primary fw-bolder">
+					Reenviar e-mail
+				</button>
+			</div>
+
+		</form>
+
+	</div>
+
+
+{{--
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
@@ -37,3 +80,4 @@
         </div>
     </x-auth-card>
 </x-guest-layout>
+--}}
